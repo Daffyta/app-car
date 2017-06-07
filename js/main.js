edd755623c60459c5bca1d7d750269709683fead
Daffyta <daffy.dine9@gmail.com>
2017-06-07 12:32:07 -0500

    Segunda pantalla - botones Log / Sign

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Timer to change index
 var cargarPagina = function(){
-    setTimeout(function(){ location.href="views/singup.html" }, 2000);
+    setTimeout(function(){ location.href="views/homepage.html" }, 2000);
 }
 $(document).ready(cargarPagina);
